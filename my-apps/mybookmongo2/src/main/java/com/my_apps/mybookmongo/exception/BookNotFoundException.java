cca47157be50bbb3f07595f679b1a9603636a97b
musaisah455 <musaisah455@gmail.com>
2026-04-11 16:20:38 +0100

    remove

--- my-apps/mybookmongo2/src/main/java/com/my_apps/mybookmongo/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.my_apps.mybookmongo.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
--- my-apps/mybookmongo2/src/main/java/com/my_apps/mybookmongo/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my_apps.mybookmongo.repository;
-
-import com.my_apps.mybookmongo.model.BookModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BookRepository extends MongoRepository<BookModel, Long> {
-
-}
