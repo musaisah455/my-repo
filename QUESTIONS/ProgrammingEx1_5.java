62265315c7a6613a9c363ed8bd71b4c8cd1e93eb
musaisah455 <musaisah455@gmail.com>
2026-07-22 22:57:55 +0100

    add

---
+++ QUESTIONS/ProgrammingEx1_5.java
@@ -0,0 +1,9 @@
+public class ProgrammingEx1_5 {
+  public static void main(String [] args) {
+    System.out.println("7.5 * 6.5 - 4.5  * 3");
+    System.out.println("--------------------");
+    System.out.println("     47.5 - 5.5");
+    System.out.println("         =");
+    System.out.println(((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5));
+  }
+}
