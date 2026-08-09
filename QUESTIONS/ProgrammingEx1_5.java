743dbcdb201f5e4420f4540b5719a96baf3346b3
musaisah455 <musaisah455@gmail.com>
2026-08-09 23:32:40 +0100

    remove

--- QUESTIONS/ProgrammingEx1_5.java
+++
@@ -1,9 +0,0 @@
-public class ProgrammingEx1_5 {
-  public static void main(String [] args) {
-    System.out.println("7.5 * 6.5 - 4.5  * 3");
-    System.out.println("--------------------");
-    System.out.println("     47.5 - 5.5");
-    System.out.println("         =");
-    System.out.println(((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5));
-  }
-}
